a433b983c89a704fc9fdc0b4eff62691b120cb88
RukeshB <rukeshbasukala@gmail.com>
2020-04-02 23:44:50 +0545

    data of all country fatched

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,68 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <h1>Home</h1>
+    <table
+      id="dtBasicExample"
+      class="table table-striped table-bordered table-sm"
+      cellspacing="0"
+      width="100%"
+    >
+      <thead>
+        <tr>
+          <th class="th-sm">Country</th>
+          <th class="th-sm">Total Case</th>
+          <th class="th-sm">New Case</th>
+          <th class="th-sm">Total Death</th>
+          <th class="th-sm">New Daath</th>
+          <th class="th-sm">Active Cases</th>
+          <th class="th-sm">Total Recovered</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-bind:key="world.id" v-for="world in worlds">
+          <td>{{world.country}}</td>
+          <td>{{world.totalCases}}</td>
+          <td>{{world.newCases}}</td>
+          <td>{{world.totalDeaths}}</td>
+          <td>{{world.newDeaths}}</td>
+          <td>{{world.activeCases}}</td>
+          <td>{{world.totalRecovered}}</td>
+        </tr>
+      </tbody>
+      <tfoot>
+        <tr>
+          <th>Country</th>
+          <th>Total Case</th>
+          <th>New Case</th>
+          <th>Total Death</th>
+          <th>New Daath</th>
+          <th>Active Cases</th>
+          <th>Total Recovered</th>
+        </tr>
+      </tfoot>
+    </table>
+    <Vue-Tabulator v-model="dados" :options="options" />
   </div>
 </template>
-
+<script src="../assets/pagination.js"></script>
 <script>
+import axios from "axios";
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  name: "HelloWorld",
+  data() {
+    return {
+      worlds: []
+    };
+  },
+  created() {
+    axios
+      .get("https://nepalcorona.info/api/v1/data/world")
+      .then(res => (this.worlds = res.data))
+      .catch(error => console.log(error));
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import VueTabulator from 'vue-tabulator';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'jquery/src/jquery.js';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import '~vue-tabulator/dist/scss/bootstrap/tabulator_bootstrap4';
+Vue.use(VueTabulator);
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+	router,
+	render : (h) => h(App)
+}).$mount('#app');
