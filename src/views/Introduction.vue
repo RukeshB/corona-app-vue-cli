c84593903ad69fada12beda0a5cca5cdb038d8c0
RukeshB <rukeshbasukala@gmail.com>
2020-04-14 21:13:26 +0545

    intoduction updated and finish of project

--- src/views/Introduction.vue
+++ src/views/Introduction.vue
@@ -1,22 +1,15 @@
 <template>
   <v-container>
-    <v-row>
-      <v-col cols="6">
-        <v-img src="../assets/corona.jpg" />
-      </v-col>
-      <v-col cols="6">
-        <h1>Introduction</h1>
-        <p>
-          Coronaviruses are a group of related viruses that cause diseases in mammals and birds.
-          In humans, coronaviruses cause respiratory tract infections that can be mild,
-          such as some cases of the common cold (among other possible causes, predominantly rhinoviruses),
-          and others that can be lethal, such as SARS, MERS, and COVID-19. Symptoms in other species vary:
-          in chickens, they cause an upper respiratory tract disease, while in cows and pigs they cause diarrhea.
-          There are yet to be vaccines or antiviral drugs to prevent or treat human coronavirus infections.
-        </p>
-      </v-col>
+    <v-row justify="center">
+      <h1>Introduction</h1>
       <p>
-        Coronaviruses constitute the subfamily Orthocoronavirinae, in the family Coronaviridae,
+        <v-img src="../assets/corona.jpg" class="leftimg" style="width:700px;height:400px;" />Coronaviruses are a group of related viruses that cause diseases in mammals and birds.
+        In humans, coronaviruses cause respiratory tract infections that can be mild,
+        such as some cases of the common cold (among other possible causes, predominantly rhinoviruses),
+        and others that can be lethal, such as SARS, MERS, and COVID-19. Symptoms in other species vary:
+        in chickens, they cause an upper respiratory tract disease, while in cows and pigs they cause diarrhea.
+        There are yet to be vaccines or antiviral drugs to prevent or treat human coronavirus infections.
+        <br />Coronaviruses constitute the subfamily Orthocoronavirinae, in the family Coronaviridae,
         order Nidovirales, and realm Riboviria. They are enveloped viruses with a positive-sense
         single-stranded RNA genome and a nucleocapsid of helical symmetry. The genome size of coronaviruses
         ranges from approximately 27 to 34 kilobases, the largest among known RNA viruses. The name
@@ -56,15 +49,15 @@
     <v-row>
       <h1>Basic protective measures against the new coronavirus</h1>
       <p>
+        <img src="../assets/Hand+wash1.png" class="rigntimg" />
         <b>Wash your hands frequently</b>
         <br />Regularly and thoroughly clean your hands with an alcohol-based
         hand rub or wash them with soap and water.
         <br />
         <b>Why?</b> Washing your hands with soap and water or using
         alcohol-based hand rub kills viruses that may be on your hands.
-      </p>
-
-      <p>
+        <br />
+        <br />
         <b>Maintain social distancing</b>
         <br />Maintain at least 1 metre (3 feet) distance between yourself and
         anyone who is coughing or sneezing.
@@ -72,17 +65,15 @@
         <b>Why?</b> When someone coughs or sneezes they spray small liquid
         droplets from their nose or mouth which may contain virus. If you are too close, you can breathe in the
         droplets, including the COVID-19 virus if the person coughing has the disease.
-      </p>
-
-      <p>
+        <br />
+        <br />
         <b>Avoid touching eyes, nose and mouth</b>
         <br />
         <b>Why?</b> Hands touch many surfaces and can pick up
         viruses. Once contaminated, hands can transfer the virus to your eyes, nose or mouth. From there, the
         virus can enter your body and can make you sick.
-      </p>
-
-      <p>
+        <br />
+        <br />
         <b>Practice respiratory hygiene</b>
         <br />Make sure you, and the people around you, follow good respiratory
         hygiene. This means covering your mouth and nose with your bent elbow or tissue when you cough or sneeze.
@@ -90,9 +81,8 @@
         <br />
         <b>Why?</b> Droplets spread virus. By following good respiratory
         hygiene you protect the people around you from viruses such as cold, flu and COVID-19.
-      </p>
-
-      <p>
+        <br />
+        <br />
         <b>If you have fever, cough and difficulty breathing, seek medical care early</b>
         <br />Stay home if you feel
         unwell. If you have a fever, cough and difficulty breathing, seek medical attention and call in advance. Follow
@@ -111,3 +101,18 @@ export default {
   name: "Introduction"
 };
 </script>
+
+<style scoped>
+.rigntimg {
+  float: right;
+  position: relative;
+}
+.leftimg {
+  float: left;
+  margin-right: 10px;
+  margin-bottom: 10px;
+}
+h1 {
+  text-align: center;
+}
+</style>
